fcab33c8df355271c68b77e09e472afadd5a0117
agilfchx <agilfachrian2@gmail.com>
2023-02-06 15:07:08 +0700

    call function getPG

--- client/pages/success.js
+++ client/pages/success.js
@@ -1,15 +1,31 @@
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import useContract from '../hooks/useContract';
+
 export default function Success() {
+  const [data, setData] = useState([]);
+  const contract = useContract();
+
+  useEffect(() => {
+    const initContract = async () => {
+      const datas = await contract.methods.getPG().call();
+      setData(datas);
+    };
+    initContract();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="flex flex-col items-center justify-center h-[35.1rem] py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
         <FontAwesomeIcon
           icon={faCheckCircle}
-          className="text-green-500 text-6xl h-24 w-24"
+          className="w-24 h-24 text-6xl text-green-500"
         />
 
-        <h1 className="text-6xl font-bold mt-4">Success!</h1>
+        <h1 className="mt-4 text-6xl font-bold">Success!</h1>
         <p className="mt-3 text-2xl">
           Your payment has been successfully processed.
         </p>
