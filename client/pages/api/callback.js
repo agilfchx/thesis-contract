0b30e356f17c8cb92eb9cf626f3c88bc9c4680d9
agilfchx <agilfachrian2@gmail.com>
2023-02-06 15:08:36 +0700

    post only and store to blockchain

--- client/pages/api/callback.js
+++ client/pages/api/callback.js
@@ -1,25 +1,29 @@
-export default function Callback(req, res) {
+import useOwner from '../../hooks/useOwner';
+
+export default async function Callback(req, res) {
   const callbackToken = '8Ytb7a33CDqPds140tmGkLeewnlF1DW8EHLljx3OKS8xMbdz';
   const xCallbackToken = req.headers['x-callback-token'];
   const httpMethod = req.method;
-  const statusXendit = [];
-
+  
   switch (httpMethod) {
-    case 'GET':
-      console.log(statusXendit);
-      res.status(200).json({ statusXendit });
-      return;
     case 'POST':
       if (xCallbackToken !== callbackToken) {
-        res.status(401).json({ error: 'Unauthorized' }); // <-- Pasti kena unauthorized kalau di hit dari client side
+        res.status(401).json({ error: 'Unauthorized' });
         return;
       } else {
-        statusXendit.push(req.body);
-        res.status(200).json({ status });
+        const badan = req.body;
+        try {
+
+          const contract = useOwner();
+          await contract.storePG(badan.external_id, badan.payment_method, badan.amount, badan.status, badan.currency, badan.payment_channel, badan.payer_email)
+        } catch(err) {
+          console.log(err)
+        }
+        res.status(200).json({ body: req.body });
       }
       break;
     default:
-      res.setHeader('Allow', ['GET', 'POST']);
+      res.setHeader('Allow', ['POST']);
       res.status(405).end(`Method ${httpMethod} Not Allowed`);
       return;
   }
